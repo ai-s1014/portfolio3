f7cc8aa29cde114f67f979901caf70ccfba9fff7
ai-s1014 <aaaip37@gmail.com>
2024-05-13 22:58:57 +0900

    “メインコンテンツ”

--- js/script.js
+++ js/script.js
@@ -37,6 +37,7 @@ const swiper1 = new Swiper('.swiper1', {
 const swiper2 = new Swiper('.swiper2', {
 	direction: 'vertical',
 	slidesPerView: 1,
+	centeredSlides: true,
 	mousewheel: true,
 	autoHeight: true,
 	speed: 1000,
@@ -53,44 +54,6 @@ const swiper2 = new Swiper('.swiper2', {
 		clickable: true,
 	},
 	effect: 'fade',
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-	var swiper;
-    // 有効化時のオプションを記述
-	function initializeSwiper() {
-		const swiper3 = new Swiper('.swiper3', {
-			loop: true,
-			autoplay: {
-				delay: 5000,
-			},
-			slidesPerView: 2,
-			speed: 1000,
-			mousewheel: true,
-		});
-	}
-
-	// 無効化する用の記述
-    function destroySwiper() {
-        if (swiper) {
-            swiper.destroy();
-            swiper = undefined;
-        }
-    }
-    // 横幅に応じて関数を実行
-    function handleResize() {
-        var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        if (w > 1024) {
-            if (!swiper) {
-                initializeSwiper();
-            }
-        } else {
-            destroySwiper();
-        }
-    }
-    // 初回実行
-    handleResize();
-    window.addEventListener('resize', handleResize);
 });
 
 // gallery
