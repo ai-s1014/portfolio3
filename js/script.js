6df8479682e64ebaadf476e0be382c60e2e5b661
ai-s1014 <aaaip37@gmail.com>
2024-05-18 22:47:19 +0900

    “js”

--- js/script.js
+++ js/script.js
@@ -1,43 +1,3 @@
-//lenis
-const lenis = new Lenis();
-
-// // モーダルを開くボタン
-// // Intersection Observerのコールバック関数
-// const callback = (entries, observer) => {
-// 	entries.forEach(entry => {
-// 	  if (entry.isIntersecting) {
-// 		// スクロールを止める
-// 		lenis.stop();
-// 	  } else {
-// 		// モーダルを再開
-// 		lenis.start();
-// 	  }
-// 	});
-//   };
-  
-//   // 監視対象の要素を取得
-//   const targetElement = document.querySelector('.c-slider');
-
-//   // Intersection Observerを作成
-//   const options = {
-// 	rootmargin: '-50% 0 0 -50%'
-// };
-//   const observer = new IntersectionObserver(callback, options);
-  
-//   // 要素を監視対象に登録
-//   observer.observe(targetElement);
-  
-
-function raf(time) {
-	lenis.raf(time);
-	requestAnimationFrame(raf);
-  }
-
-  requestAnimationFrame(raf);
-
-
-
-
 // header
 $('.menu-btn').on('click', function() {
 	$(this).toggleClass('open');
@@ -58,26 +18,27 @@ $(window).on('scroll', function() {
 });
 
 //scroll-container 要素拡大
-$('.gallery').each(function(){
+$(document).ready(function() {
 	var $win = $(window),
-		  $winH = $win.height(),
-		  $connect = $(this),
-		  position = $connect.offset().top,
-		  current = 0,
-		  scroll;
-	$win.on('load scroll', function(){
-		  scroll = $win.scrollTop();
-		  current = Math.max(0, (1 - (position - scroll) / $winH) * 2);
-
-      if (scroll > position - $winH) {
-			  $connect.css('transform', 'scale(' + current + ')');
-		  }
-		  if (current > 1) {
-			  current = 1;
-		  }
+		$winH = $win.height(),
+		$connect = $('.gallery-wrapper'),
+		position = $connect.offset().top,
+		current = 0,
+		scroll;
+  
+	$win.on('scroll', function() {
+	  scroll = $win.scrollTop();
+	  current = Math.max(1, (1 - (position - scroll) / $winH) * 2);
+  
+	  if (scroll > position - $winH) {
+		$connect.css('transform', 'scale(' + current + ')');
+	  }
+	  if (current > 1) {
+		current = 1;
+	  }
 	});
-});
-
+  });
+  
 
 // swiper
 const swiper1 = new Swiper('.swiper1', {
